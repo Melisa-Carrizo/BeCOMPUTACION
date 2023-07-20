38f15199816d8c38953361018bf11f6368db5209
Melisa Carrizo <melisacarrizo5@gmail.com>
2023-07-19 23:00:25 -0300

    move cartwidget

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -24,7 +24,7 @@ const NavBar = ()=> {
                         </ul>
                     </li>
                 </ul>
-                    <div>
+                    <div className="ms-auto">
                         <CartWidget/>
                     </div>
                 </div>
